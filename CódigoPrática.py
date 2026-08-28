25c59caad405ce9551003e8a2a638475709c423c
Yasmin All <okimadelalua@gmail.com>
2026-08-28 16:47:22 -0400

    Add files via upload

---
+++ CódigoPrática.py
@@ -0,0 +1,1 @@
+#Este é o código em python, prova com o tema em PenhaS, este código foi baseado em um diagrama de classes feito no Astah e em sala de aula em grupo formado por: Annaly, Yasmin, Larissa e Júlia.
