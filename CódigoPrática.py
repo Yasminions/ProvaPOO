3ed1621731c4c1a042021ee1fbbd7e041cdfd9ad
Yasmin All <okimadelalua@gmail.com>
2026-08-28 17:23:17 -0400

    Alteração nos nomes

--- CódigoPrática.py
+++ CódigoPrática.py
@@ -1,1 +1,2 @@
-#Este é o código em python, prova com o tema em PenhaS, este código foi baseado em um diagrama de classes feito no Astah e em sala de aula em grupo formado por: Annaly, Yasmin, Larissa e Júlia.
+#Este é o código em python, prova com o tema em PenhaS, este código foi baseado em um diagrama de classes feito no Astah e em sala de aula em grupo formado por: Annaly Lima, Yasmin Almeida, Larissa De Castro e Júlia Do Carmo.
+
