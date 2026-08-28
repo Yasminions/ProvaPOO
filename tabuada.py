99a010577970f0a3d2d296827a38163426e9fa4d
Yasmin All <okimadelalua@gmail.com>
2026-08-28 15:27:22 -0400

    Deletando teste tabuada.py
    Retirando arquivo de teste de conexão do github e vs code

--- tabuada.py
+++
@@ -1,8 +0,0 @@
-print('tabuada')
-numero=int(input("Insira um número: "))
-for indice in range(11):
-    print(numero, 'x', indice,'=',numero*indice)
-    #print(str(numero)+'x'+str(indice))
-    #print(f'{numero} x {indice} = {numero*indice}')
-    
-#Opções acima ao inves da linha de cima 
