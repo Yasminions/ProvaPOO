ff6967b314f654b9cc56016a67522e482bc510f0
Yasmin All <okimadelalua@gmail.com>
2026-09-04 20:47:27 -0300

    Adição de classe

--- CódigoPrática.py
+++ CódigoPrática.py
@@ -1,2 +1,15 @@
 #Este é o código em python, prova com o tema em PenhaS, este código foi baseado em um diagrama de classes feito no Astah e em sala de aula em grupo formado por: Annaly Lima, Yasmin Almeida, Larissa De Castro e Júlia Do Carmo.
 
+#Essa é a classe Cadastro:
+class Cadastro:
+    def __init__(self, nome: str, email: str, senha: str, cep: int, cpf: int, apelido: str, genero: bool, raca: bool, data_de_nascimento: int, questionario: bool):
+        self._nome = nome
+        self._email = email
+        self._senha = senha
+        self._cep = cep
+        self._cpf = cpf
+        self._apelido = apelido
+        self._genero = genero
+        self._raca = raca
+        self._data_de_nascimento = data_de_nascimento
+        self._questionario = questionario
